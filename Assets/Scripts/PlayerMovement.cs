a979828df0cd089e2f59065d5aee32999cb6c850
NullPerson <edu1khoory@hotmail.com>
2025-08-06 11:45:18 +0400

    Adding more dialogue

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody _rb;
 
     [SerializeField] private float speed;
+    [SerializeField] private float speed2;
 
     private float hMovement;
-    //private float vMovement;
+    private float vMovement;
 
 
     private void Awake()
@@ -20,13 +21,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+
+        Vector3 front = transform.TransformDirection(Vector3.forward);
         //movemeent mechanic
         hMovement = Input.GetAxisRaw("Horizontal");
-        //vMovement = Input.GetAxisRaw("Vertical");
+        vMovement = Input.GetAxisRaw("Vertical");
 
         Quaternion turning = Quaternion.Euler(0f, hMovement * speed * Time.deltaTime, 0f);
 
         _rb.MoveRotation(_rb.rotation * turning);
+        _rb.AddForce(front * speed2 * vMovement, ForceMode.Force);
 
 
 
